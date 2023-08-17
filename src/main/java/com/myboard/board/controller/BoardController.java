054c1507381d3ab973227e1adfa9d73e21fd3086
jangheepark <jeanparkmk@gmail.com>
2023-08-18 08:55:38 +0900

    [#1] feat: BaordController createBoard 메서드 구현 - BoardService와 의존관계를 생성 - Board 저장 후 생성된 Board의 상세 페이지로 Redirect (TODO)

--- src/main/java/com/myboard/board/controller/BoardController.java
+++ src/main/java/com/myboard/board/controller/BoardController.java
@@ -1,19 +1,36 @@
 package com.myboard.board.controller;
 
 import com.myboard.board.dto.BoardDto;
+import com.myboard.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RequestMapping("/board")
 @Controller
 public class BoardController {
 
+    private final BoardService boardService;
+
     public String boardCreateForm(Model model) {
         model.addAttribute("board", new BoardDto.CreateRequest());
 
         return "board/createForm";
+    }
+
+    @PostMapping
+    public String createBoard(
+            @ModelAttribute BoardDto.CreateRequest dto,
+            @RequestParam MultipartFile thumbnail
+    ) {
+        Long boardId = boardService.save(dto, thumbnail);
+
+        return "redirect:/board/" + boardId;
     }
 }
