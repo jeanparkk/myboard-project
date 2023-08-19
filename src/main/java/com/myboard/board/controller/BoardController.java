92ef1cfa8286d166a56f34ff18840fe981858f64
jangheepark <jeanparkmk@gmail.com>
2023-08-19 09:05:57 +0900

    [#1] feat: 게시글 생성 요청 시 유효성 검사 추가 - title은 필수 입력 값, 빈 문자, 공백 문자 허용하지 않음. 길이는 최대 255자 - content는 필수 입력 값, 빈 문자, 공백 문자 허용하지 않음. 길이는 최대 20000자

--- src/main/java/com/myboard/board/controller/BoardController.java
+++ src/main/java/com/myboard/board/controller/BoardController.java
@@ -5,11 +5,14 @@ import com.myboard.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/board")
@@ -26,9 +29,13 @@ public class BoardController {
 
     @PostMapping
     public String createBoard(
-            @ModelAttribute BoardDto.CreateRequest dto,
+            @Valid @ModelAttribute("board") BoardDto.CreateRequest dto,
+            BindingResult bindingResult,
             @RequestParam MultipartFile thumbnail
     ) {
+        if (bindingResult.hasErrors()) {
+            return "board/createForm";
+        }
         Long boardId = boardService.save(dto, thumbnail);
 
         return "redirect:/board/" + boardId;
