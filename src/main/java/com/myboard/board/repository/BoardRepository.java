092e4cad96f758a3422828ddad0f6cbecdfed169
jangheepark <jeanparkmk@gmail.com>
2023-08-23 11:00:40 +0900

    [#3] feat: Pageable, Category, Status를 받아 Page를 반환받는 Repository 메서드 추가 - Category와 Status 그리고 Pageable를 받아서 Where절로 쿼리를 수행한 결과를 Page 객체로 반환

--- src/main/java/com/myboard/board/repository/BoardRepository.java
+++ src/main/java/com/myboard/board/repository/BoardRepository.java
@@ -2,7 +2,12 @@ package com.myboard.board.repository;
 
 
 import com.myboard.board.domain.Board;
+import com.myboard.type.Category;
+import com.myboard.type.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    Page<Board> findAllByCategoryAndStatus(Category category, Status status, Pageable pageable);
 }
