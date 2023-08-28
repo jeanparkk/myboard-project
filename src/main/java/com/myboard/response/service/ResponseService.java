571fc02261794b67a0e509e839a3194eed929b7f
jangheepark <jeanparkmk@gmail.com>
2023-08-28 12:25:28 +0900

    [#5] feat: register, authConfirm html작성, ErrorCode, SuccessCode enum작성

--- src/main/java/com/myboard/response/service/ResponseService.java
+++ src/main/java/com/myboard/response/service/ResponseService.java
@@ -3,6 +3,7 @@ package com.myboard.response.service;
 import com.myboard.response.dto.CommonResult;
 import com.myboard.response.dto.ListResult;
 import com.myboard.response.dto.SingleResult;
+import com.myboard.response.type.ErrorCode;
 
 import java.util.List;
 
