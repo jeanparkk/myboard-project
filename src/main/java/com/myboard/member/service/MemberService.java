25a85b48976ecb9481f7d90aa18b53aa90f214a4
jangheepark <jeanparkmk@gmail.com>
2023-08-29 11:49:38 +0900

    [#5] feat: MemberService 작성

--- src/main/java/com/myboard/member/service/MemberService.java
+++ src/main/java/com/myboard/member/service/MemberService.java
@@ -1,4 +1,12 @@
 package com.myboard.member.service;
 
+import com.myboard.member.dto.MemberRegistration;
+import com.myboard.response.dto.CommonResult;
+import com.myboard.response.dto.SingleResult;
+
 public interface MemberService {
+
+    SingleResult<Long> register(MemberRegistration parameter);
+
+    CommonResult authConfirm(Long id, String emailAuthToken);
 }
